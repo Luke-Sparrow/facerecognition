6db8c0b2891c64c1d3b8563293d938964238da4f
Luke Sparrow <luke.sparrow@pm.me>
2020-08-18 17:45:18 -0600

    Updates

---
+++ precache-manifest.002ec21b225ead8b2e419fb884ecd8c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2754572625207a160076f85cf56091ae",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "9225668a5efea3a6295e",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9938f24af172b1f7ae89",
+    "url": "/facerecognition/static/css/main.eaad08ed.chunk.css"
+  },
+  {
+    "revision": "9225668a5efea3a6295e",
+    "url": "/facerecognition/static/js/2.6b487638.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognition/static/js/2.6b487638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9938f24af172b1f7ae89",
+    "url": "/facerecognition/static/js/main.96f2cc3b.chunk.js"
+  },
+  {
+    "revision": "fa6a11a9cd7eebd80802",
+    "url": "/facerecognition/static/js/runtime-main.1ad3eaac.js"
+  }
+]);
